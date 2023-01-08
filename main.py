fe4b01e4735781e8415706b534c580f0aee2a441
Brian Thorne <brian@thorne.link>
2023-01-08 16:36:42 +1300

    Renaming netcheck to netchecks

--- main.py
+++ main.py
@@ -376,7 +376,7 @@ def create_job_object(job_name: str, job_spec):
 
 def get_common_labels(name):
     return {
-        'app.kubernetes.io/name': 'netcheck',
+        'app.kubernetes.io/name': 'netchecks',
         'app.kubernetes.io/version': VERSION,
         'app.kubernetes.io/instance': name
     }
@@ -386,7 +386,7 @@ def create_job_spec(name, cm: V1ConfigMap, template_overides: dict = None):
     # Container template first
     container = client.V1Container(
         name="netcheck",
-        image="ghcr.io/hardbyte/netcheck:main",
+        image="ghcr.io/netchecks/netchecks:main",
         image_pull_policy="Always",  # Until we pin versions
         command=["poetry", "run", "netcheck", "run", "--config", "/netcheck/rules.json"],
         volume_mounts=[
@@ -459,69 +459,4 @@ def apply_overrides(template, overrides: dict):
 
     _apply_overrides(template, overrides)
     return template
-
-# def main():
-#     config.load_kube_config()
-#
-#     api = client.CustomObjectsApi()
-#
-#     # it's my custom resource defined as Dict
-#     my_resource = {
-#         "apiVersion": "stable.example.com/v1",
-#         "kind": "CronTab",
-#         "metadata": {"name": "my-new-cron-object"},
-#         "spec": {
-#             "cronSpec": "* * * * */5",
-#             "image": "my-awesome-cron-image"
-#         }
-#     }
-#
-#     # patch to update the `spec.cronSpec` field
-#     patch_body = {
-#         "spec": {"cronSpec": "* * * * */10", "image": "my-awesome-cron-image"}
-#     }
-#
-#     # create the resource
-#     api.create_namespaced_custom_object(
-#         group="stable.example.com",
-#         version="v1",
-#         namespace="default",
-#         plural="crontabs",
-#         body=my_resource,
-#     )
-#     print("Resource created")
-#
-#     # get the resource and print out data
-#     resource = api.get_namespaced_custom_object(
-#         group="stable.example.com",
-#         version="v1",
-#         name="my-new-cron-object",
-#         namespace="default",
-#         plural="crontabs",
-#     )
-#     print("Resource details:")
-#     pprint(resource)
-#
-#     # patch the namespaced custom object to update the `spec.cronSpec` field
-#     patch_resource = api.patch_namespaced_custom_object(
-#         group="stable.example.com",
-#         version="v1",
-#         name="my-new-cron-object",
-#         namespace="default",
-#         plural="crontabs",
-#         body=patch_body,
-#     )
-#     print("Resource details:")
-#     pprint(patch_resource)
-#
-#     # delete it
-#     api.delete_namespaced_custom_object(
-#         group="stable.example.com",
-#         version="v1",
-#         name="my-new-cron-object",
-#         namespace="default",
-#         plural="crontabs",
-#         body=client.V1DeleteOptions(),
-#     )
-#     print("Resource deleted")
 
