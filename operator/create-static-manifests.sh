f65d2be4361a7d4cdc73457e3ab48d1a73140f97
Denis Khoshaba <Deniskhoshaba@gmail.com>
2023-04-20 08:31:20 +1000

    add shellcheck and hadolint to CI + ensure they pass (#14)
    Co-authored-by: Brian Thorne <brian@hardbyte.nz>

--- operator/create-static-manifests.sh
+++ operator/create-static-manifests.sh
@@ -2,23 +2,21 @@
 # Generates the /manifests/deploy.yaml
 
 if [ -n "$DEBUG" ]; then
-	set -x
+  set -x
 fi
 
 #set -o errexit
 set -o nounset
 set -o pipefail
 
+cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P
 
-cd $(dirname "${BASH_SOURCE}") && pwd -P
-
-MANIFEST=manifests/deploy.yaml
+MANIFEST="manifests/deploy.yaml"
 
 helm template netchecks-operator ./charts/netchecks \
   --values examples/kind-installation/values.yaml \
   --namespace netchecks \
-  > $MANIFEST
+  > "${MANIFEST}"
 
-sed -i.bak '/app.kubernetes.io\/managed-by: Helm/d' $MANIFEST
-sed -i.bak '/helm.sh/d' $MANIFEST
-
+sed -i.bak '/app.kubernetes.io\/managed-by: Helm/d' "${MANIFEST}"
+sed -i.bak '/helm.sh/d' "${MANIFEST}"
