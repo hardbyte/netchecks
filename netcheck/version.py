2fba4eff278b850f3303c7cbad09faa847e16089
Brian Thorne <brian@thorne.link>
2023-05-15 16:30:43 +1200

    Remove implicit dependency on setuptools

--- netcheck/version.py
+++ netcheck/version.py
@@ -1,9 +1,8 @@
-import pkg_resources
-
+from importlib import metadata
 
 try:
-    NETCHECK_VERSION = pkg_resources.get_distribution("netcheck").version
-except pkg_resources.DistributionNotFound:
+    NETCHECK_VERSION =  metadata.version("netcheck")
+except metadata.PackageNotFoundError:
     NETCHECK_VERSION = "unknown"
 
 OUTPUT_JSON_VERSION = "dev"  # set to v1 once stable
