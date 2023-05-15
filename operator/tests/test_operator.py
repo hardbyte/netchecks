8259926d2d335bf8068d7cf510b15bcc568b319e
Brian Thorne <brian@thorne.link>
2023-05-15 19:05:17 +1200

    kopf testing

--- operator/tests/test_operator.py
+++ operator/tests/test_operator.py
@@ -5,19 +5,19 @@ from kopf.testing import KopfRunner
 
 def test_operator(netchecks_crds, k8s_namespace, test_file_path):
     with KopfRunner(["run", "-A", "netchecks_operator/main.py"]) as runner:
-        # do something while the operator is running.
+        # Remove all existing network assertions
         subprocess.run(
-            f"kubectl delete -f {test_file_path('http-job.yaml')} -n {k8s_namespace}",
+            f"kubectl delete -A NetworkAssertions --all",
             shell=True,
         )
-        time.sleep(1)  # give it some time to react
+        time.sleep(3.0)  # give it some time to react
 
         subprocess.run(
             f"kubectl apply -f {test_file_path('http-job.yaml')} -n {k8s_namespace}",
             shell=True,
             check=True,
         )
-        time.sleep(10)  # give it some time to react and to sleep and to retry
+        time.sleep(10)  # allow operator to react (pull images, retry once)
 
         list_assertions_response = subprocess.run(
             f"kubectl get networkassertions -n {k8s_namespace}", shell=True, check=True
@@ -31,7 +31,7 @@ def test_operator(netchecks_crds, k8s_namespace, test_file_path):
             shell=True,
             check=True,
         )
-        time.sleep(1)  # give it some time to react
+        time.sleep(3.0)  # give it some time to react
 
     assert runner.exit_code == 0
     assert runner.exception is None
