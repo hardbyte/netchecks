55e19c9e2f576f2679e24fbd1f721f56f7eab598
Brian Thorne <brian@thorne.link>
2023-06-22 13:59:36 +1200

    🎨 Update logo and favicons

--- docs/src/pages/_app.jsx
+++ docs/src/pages/_app.jsx
@@ -65,14 +65,19 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>{pageTitle}</title>
-        {description && <meta name="description" content={description} />}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        {description && <meta name="description" content={description} /> }
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000"/>
-        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
         <meta name="theme-color" content="#ffffff"/>
+
       </Head>
       <Layout title={title} tableOfContents={tableOfContents}>
         <Component {...pageProps} />
