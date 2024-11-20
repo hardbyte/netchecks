ed29bb164785ca80d96096e1c234220f862e7cb5
Brian Thorne <brian@thorne.link>
2024-11-20 23:30:08 +1300

    🦀 WIP - switch CEL implementation

--- netcheck/validation.py
+++ netcheck/validation.py
@@ -2,10 +2,10 @@ import base64
 import json
 import logging
 from typing import Dict
-
-import celpy
 import yaml
-from celpy import CELParseError, CELEvalError, json_to_cel
+
+from cel import cel
+
 
 logger = logging.getLogger("netcheck.validation")
 
@@ -31,35 +31,23 @@ def evaluate_cel_with_context(context: Dict, validation_rule: str):
     Raises:
         ValueError: If the CEL expression is invalid.
     """
-
-    env = celpy.Environment()
-
-    # Validate the CEL validation rule and compile to ast
-    try:
-        ast = env.compile(validation_rule)
-    except CELParseError:
-        print("Invalid CEL expression. Treating as error.")
-        raise ValueError("Invalid CEL expression")
-
-    # create the CEL program
     functions = {
-        "parse_json": lambda s: json_to_cel(json.loads(s)),
-        "parse_yaml": lambda s: json_to_cel(yaml.safe_load(s)),
+        "parse_json": lambda s: json.loads(s),
+        "parse_yaml": lambda s: yaml.safe_load(s),
         "b64decode": lambda s: base64.b64decode(s).decode("utf-8"),
         "b64encode": lambda s: base64.b64encode(s.encode()).decode(),
     }
-    prgm = env.program(ast, functions=functions)
-
-    # Set up the context
-    activation = celpy.json_to_cel(context)
+    env = cel.Context(
+        variables=context,
+        functions=functions,
+    )
 
     # Evaluate the CEL expression
     try:
-        context = prgm.evaluate(activation)
-    except CELEvalError:
+        result = cel.evaluate(validation_rule, env)
+    except Exception as e:
         # Note this can fail if the context is missing a key e.g. the probe
         # failed to return a value for a key that the validation rule expects
-
         return False
 
-    return context
+    return result
